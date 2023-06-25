08a3c32798793901a2998a48fd4cecb5d1882278
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-06-25 13:45:02 +0200

    create user

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='searchForm'>
+        <input type='text' placeholder='Find a user' />
+      </div>
+      <div className='userChat'>
+        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
+        <div className='userChatInfo'>
+          <span>Ann Marie</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
