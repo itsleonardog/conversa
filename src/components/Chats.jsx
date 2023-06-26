d919f5f588ab93faf1d1b26af18b3c3b2c7e108a
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-06-26 19:40:20 +0200

    messages layout

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className='userChat'>
+        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
+        <div className='userChatInfo'>
+          <span>Ann Marie</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className='userChat'>
+        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
+        <div className='userChatInfo'>
+          <span>Ann Marie</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className='userChat'>
+        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
+        <div className='userChatInfo'>
+          <span>Ann Marie</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
