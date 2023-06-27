aeaf4323c2d3f86a3d622a72fcc6b84cea0a1b73
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-06-27 11:41:11 +0200

    chat layout terminated

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Attach from '../img/attach.png'
+import Img from '../img/img.png'
+
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='Type something...' />
+      <div className='send'>
+        <img src={Attach} alt='' />
+        <input type='file' style={{display:'none'}} id='file' />
+        <label>
+          <img src={Img} alt='' />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className='messageInfo'>
+        <img
+          src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+          alt=''
+        />
+        <span>Just now</span>
+      </div>
+      <div className='messageContent'>
+        <p>Heyo</p>
+        <img
+        src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80'
+        alt=''
+        />
+      </div>
+    </div>
   )
 }
 
