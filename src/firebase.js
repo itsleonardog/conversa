b7b679137ca77a6c662482e992e5d9c43518ad73
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-06-27 16:14:02 +0200

    database set. issue to solve

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBD73EG2bxXc3owo9WeaT8YFwwh_IAfrmg",
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
+export const storage = getStorage()
+export const db = getFirestore()
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import Add from '../img/addAvatar.png'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth, storage } from '../firebase'
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+
 
 const Register = () => {
   const [err, setErr] = useState(false)
@@ -14,6 +17,39 @@ const Register = () => {
 
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password)
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      // 1. 'state_changed' observer, called any time the state changes
+      // 2. Error observer, called on failure
+      // 3. Completion observer, called on successful completion
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            })
+
+            await setDoc(doc(db, 'users', res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            })
+          });
+        }
+      );
+
+
     } catch(err) {
       setErr(true)
     }
