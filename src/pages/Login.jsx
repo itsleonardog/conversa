709aec78ddacbb138021a4253baade3e42c02ac4
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-07-01 18:26:18 +0200

    react router dom protected route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import Add from '../img/addAvatar.png'
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const email = e.target[0].value
+    const password = e.target[1].value
+
+    try {
+
+    } catch(err) {
+      console.error(err);
+      setErr(true)
+    }
+  }
+
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
