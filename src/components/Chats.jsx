ea8aa7d4ec712208727e70a990e23c8a7c4a93f6
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-07-07 16:50:15 +0200

    fetch chat conversations

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -21,33 +21,17 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  console.log(chats)
-
   return (
     <div className='chats'>
-      <div className='userChat'>
-        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
-        <div className='userChatInfo'>
-          <span>Ann Marie</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map(chat => (
+        <div className='userChat' key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt='' />
+          <div className='userChatInfo'>
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-
-      <div className='userChat'>
-        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
-        <div className='userChatInfo'>
-          <span>Ann Marie</span>
-          <p>Hello</p>
-        </div>
-      </div>
-
-      <div className='userChat'>
-        <img src='https://images.unsplash.com/photo-1496302662116-35cc4f36df92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80' alt='' />
-        <div className='userChatInfo'>
-          <span>Ann Marie</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
