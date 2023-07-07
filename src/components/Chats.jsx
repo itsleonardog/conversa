bdec4c4ef3688a3e295e9eb349762ba186eec136
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-07-07 17:54:23 +0200

    fetch private chat message

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,10 +3,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { db } from '../firebase'
 // import { currentUser } from 'firebase/auth'
 import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext'
 
 const Chats = () => {
   const [chats, setChats] = useState([])
   const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
 
   useEffect(() => {
     const getChats = () => {
@@ -21,10 +23,14 @@ const Chats = () => {
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
+  const handleSelect = (u) => {
+    dispatch({ type:'CHANGE_USER', payload: u });
+  }
+
   return (
     <div className='chats'>
       {Object.entries(chats)?.map(chat => (
-        <div className='userChat' key={chat[0]}>
+        <div className='userChat' key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)}>
           <img src={chat[1].userInfo.photoURL} alt='' />
           <div className='userChatInfo'>
             <span>{chat[1].userInfo.displayName}</span>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Attach from '../img/attach.png'
 import Img from '../img/img.png'
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
 
 
 const Input = () => {
+  const [text, setText] = useState('');
+  const [img, setImg] = useState(null);
+
+  const {currentUser} = useContext(AuthContext);
+  const {data} = useContext(ChatContext);
+
+  const handleSend = () => {
+
+  }
+
   return (
     <div className='input'>
-      <input type='text' placeholder='Type something...' />
+      <input type='text' placeholder='Type something...' onChange= { e => setText(e.target.value)} />
       <div className='send'>
         <img src={Attach} alt='' />
-        <input type='file' style={{display:'none'}} id='file' />
+        <input type='file' style={{display:'none'}} id='file' onChange={ e => setImg(e.target.files[0])} />
         <label>
           <img src={Img} alt='' />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   )
